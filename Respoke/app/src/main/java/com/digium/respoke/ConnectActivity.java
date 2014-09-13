03fc0c91dbdf36455f982025385b4f89f29c8107
Jason Adams <jason@ninjanetic.com>
2014-09-13 17:12:34 -0400

    Create SDK library project. Implement debug get token API call. Skeleton connect screen.

--- Respoke/app/src/main/java/com/digium/respoke/ConnectActivity.java
+++ Respoke/app/src/main/java/com/digium/respoke/ConnectActivity.java
@@ -4,14 +4,30 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.digium.respokesdk.Respoke;
+import com.digium.respokesdk.RespokeClient;
+import com.digium.respokesdk.RestAPI.*;
 
 
 public class ConnectActivity extends Activity {
+
+    private EditText endpointTextBox = null;
+    private EditText groupTextBox = null;
+    private APIGetToken apiGetToken = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
+
+        endpointTextBox = (EditText)findViewById(R.id.editText1);
+        groupTextBox = (EditText)findViewById(R.id.editText2);
     }
 
 
@@ -21,6 +37,7 @@ public class ConnectActivity extends Activity {
         getMenuInflater().inflate(R.menu.connect, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -32,5 +49,15 @@ public class ConnectActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void connect(View view) {
+        String endpointID = endpointTextBox.getText().toString();
+        String groupID = groupTextBox.getText().toString();
+        String appID = "2b446810-6d92-4fa4-826a-2eabced82d60";
+
+        RespokeClient client = Respoke.sharedInstance().createClient();
+        client.connect(endpointID, appID, true, null, this.getApplicationContext());
     }
 }
